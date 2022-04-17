781643d2c408a50fd3ac24c588d0efd3b793caaf
arifulhasantotul <mdahtotul@gmail.com>
2022-04-18 01:35:37 +0600

    left_side_login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,15 @@
 import React from "react";
+import SideImage from "../../images/phone.svg";
+import LoginCss from "./Login.module.css";
 
 const Login = () => {
   return (
-    <div>
-      <h2>Login</h2>
+    <div className={LoginCss.page_wrapper_login}>
+      {/* left side of the login page */}
+      <div className={LoginCss.page_login_left}>
+        <img className={LoginCss.left_logo} src={SideImage} alt="login logo" />
+      </div>
+      <div className={LoginCss.page_login_right}>div</div>
     </div>
   );
 };
