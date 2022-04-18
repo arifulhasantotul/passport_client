4ec51affb3d37511bf122fd74cab334239aca38b
arifulhasantotul <mdahtotul@gmail.com>
2022-04-18 15:57:51 +0600

    frontend responsive

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -4,54 +4,54 @@ import * as FcIcons from "react-icons/fc";
 import { Link } from "react-router-dom";
 import LogoImage from "../../images/logo.svg";
 import SideImage from "../../images/phone.svg";
-import LoginCss from "./Login.module.css";
+import LoginCSS from "./Login.module.css";
 
 const Login = () => {
   return (
-    <div className={LoginCss.page_wrapper_login}>
+    <div className={LoginCSS.page_wrapper_login}>
       {/* left side of the login page */}
-      <div className={LoginCss.page_login_left}>
+      <div className={LoginCSS.page_login_left}>
         {/* left side image */}
-        <img className={LoginCss.left_logo} src={SideImage} alt="login logo" />
+        <img className={LoginCSS.left_logo} src={SideImage} alt="login logo" />
       </div>
-      <div className={LoginCss.page_login_right}>
+      <div className={LoginCSS.page_login_right}>
         {/* form wrapper */}
-        <div className={LoginCss.login_form_wrapper}>
+        <div className={LoginCSS.login_form_wrapper}>
           {/* logo */}
           <img src={LogoImage} alt="Pioneer Alpha logo" />
-          <p className={LoginCss.subtitle}>Explore new courses... Hurry up!!</p>
+          <p className={LoginCSS.subtitle}>Explore new courses... Hurry up!!</p>
           {/* login fields */}
-          <div className={LoginCss.login_field}>
+          <div className={LoginCSS.login_field}>
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="email"
               placeholder="Email Address"
             />
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="password"
               placeholder="Password"
             />
             <input
-              className={LoginCss.submit_btn}
+              className={LoginCSS.submit_btn}
               type="button"
               value="LOG IN"
             />
             <hr />
-            <span className={LoginCss.or}>or</span>
+            <span className={LoginCSS.or}>or</span>
           </div>
 
-          <div className={LoginCss.third_party_login}>
-            <span className={LoginCss.third_icon}>
-              <BsIcons.BsFacebook className={LoginCss.blue} />
+          <div className={LoginCSS.third_party_login}>
+            <span className={LoginCSS.third_icon}>
+              <BsIcons.BsFacebook className={LoginCSS.blue} />
             </span>
-            <span className={LoginCss.third_icon}>
+            <span className={LoginCSS.third_icon}>
               {" "}
               <FcIcons.FcGoogle />
             </span>
           </div>
 
-          <p className={LoginCss.account}>
+          <p className={LoginCSS.account}>
             Don't have an account? <Link to="/register">Sign Up</Link>
           </p>
         </div>
--- src/pages/Login/Register.js
+++ src/pages/Login/Register.js
@@ -2,54 +2,54 @@ import React from "react";
 import { Link } from "react-router-dom";
 import LogoImage from "../../images/logo.svg";
 import SideImage from "../../images/phone.svg";
-import LoginCss from "./Login.module.css";
+import LoginCSS from "./Login.module.css";
 
 const Register = () => {
   return (
-    <div className={LoginCss.page_wrapper_login}>
+    <div className={LoginCSS.page_wrapper_login}>
       {/* left side of the login page */}
-      <div className={LoginCss.page_login_left}>
+      <div className={LoginCSS.page_login_left}>
         {/* left side image */}
-        <img className={LoginCss.left_logo} src={SideImage} alt="login logo" />
+        <img className={LoginCSS.left_logo} src={SideImage} alt="login logo" />
       </div>
-      <div className={LoginCss.page_login_right}>
+      <div className={LoginCSS.page_login_right}>
         {/* form wrapper */}
-        <div className={LoginCss.login_form_wrapper}>
+        <div className={LoginCSS.login_form_wrapper}>
           {/* logo */}
           <img src={LogoImage} alt="Pioneer Alpha logo" />
-          <p className={LoginCss.subtitle}>
+          <p className={LoginCSS.subtitle}>
             Courses are waiting for you... Hurry up!!
           </p>
           {/* login fields */}
-          <div className={LoginCss.login_field}>
+          <div className={LoginCSS.login_field}>
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="text"
               placeholder="Name"
             />
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="email"
               placeholder="Email Address"
             />
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="tel"
               placeholder="Phone"
             />
             <input
-              className={LoginCss.login_input}
+              className={LoginCSS.login_input}
               type="password"
               placeholder="Password"
             />
             <input
-              className={LoginCss.submit_btn}
+              className={LoginCSS.submit_btn}
               type="button"
               value="LOG IN"
             />
           </div>
 
-          <p className={LoginCss.account}>
+          <p className={LoginCSS.account}>
             Already have an account? <Link to="/">Sign In</Link>
           </p>
         </div>
