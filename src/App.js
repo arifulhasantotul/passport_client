e9075bd9bdd6be9fc8026e533b24fbf0eb47931f
arifulhasantotul <mdahtotul@gmail.com>
2022-04-18 22:36:45 +0600

    route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import "./App.css";
+import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Register from "./pages/Login/Register";
 
 function App() {
+  const user = false;
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/home" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route
+            path="/"
+            element={user ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route
+            path="/login"
+            element={user ? <Navigate to="/home" /> : <Login />}
+          />
+          <Route path="/home" element={user ? <Home /> : <Navigate to="/" />} />
+          <Route path="/register" element={user ? <Home /> : <Register />} />
         </Routes>
       </Router>
     </>
